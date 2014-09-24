6bf7de6309763359f98d9f673c39d5b1456619fe
Veers <a.konviser@gmail.com>
2014-09-24 05:45:25 +0400

    init_commit2

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        public enum DayOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+
+        public static IEnumerable<IEnumerable<T>> Split<T>(IEnumerable<T> source, DayOfWeek firstDay)
+        {
+            var part = new List<T>();
+
+            foreach (var item in source)
+            {
+                if (item.ToString() != firstDay.ToString())
+                    part.Add(item);
+
+                if (item.ToString() == firstDay.ToString())
+                {
+                    yield return part;
+                    part = new List<T>();
+                    part.Add(item);
+                }
+            }
+
+            if (part.Count > 0)
+                yield return part;
+        }
+
+        static IEnumerable<DayOfWeek> GetSortedDayOfWeek(DayOfWeek firstDayOfWeek)
+        {
+            var parts = Split<DayOfWeek>((IEnumerable<DayOfWeek>)Enum.GetValues(typeof(DayOfWeek)), firstDayOfWeek);
+
+            IEnumerable<DayOfWeek> result = new List<DayOfWeek>();
+
+            foreach (var part in parts)
+                if (parts.Last().Count() == part.Count())
+                    result = part.Concat(parts.First());
+
+            return result;
+            //throw new NotImplementedException();
+        }
+        static void Main(string[] args)
+        {
+            var result = GetSortedDayOfWeek(DayOfWeek.Friday);
+            result.ToList().ForEach(i => Console.WriteLine(i));
+            Console.ReadKey();
+        }
+    }
+}
